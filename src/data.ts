c5a289e52f0e4bb89d912633bf0f7eeea76d3e90
Jeongwoo Park <wahoo9040@naver.com>
2024-12-05 16:15:52 +0900

    docs: 옵저버 패턴과 Tanstack Query에서의 옵저버 패턴 글 작성

--- src/data.ts
+++ src/data.ts
@@ -7,12 +7,13 @@ export const posts: Record<PostType, PostPreview[]> = {
       fileName: "Possibility-of-Macro-Tasks-Being-Executed-Before-Paint-and-React-useEffect",
       date: "2024-11-28",
     },
-    // {
-    //   id: "2",
-    //   type: "Writing",
-    //   title: "[Apollo Basic] Apollo Client를 이용하여 Supabase GraphQL 요청 보내기",
-    //   date: "2024-11-27",
-    // },
+    {
+      id: "2",
+      type: "Writing",
+      title: "옵저버 패턴과 Tanstack Query에서의 옵저버 패턴",
+      fileName: "Observer-Pattern-and-the-Use-of-Observer-Pattern-in-TanStack-Query",
+      date: "2024-04-08",
+    },
   ],
   Reading: [
     // { id: "1", type: "Reading", title: "용서할 줄 안다는 것", date: "2024-9-15" }
