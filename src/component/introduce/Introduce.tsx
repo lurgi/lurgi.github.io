55d64d4cae007e2a5443f6b1f505b2a7f459dc32
Jeongwoo Park <wahoo9040@naver.com>
2024-12-06 18:58:43 +0900

    feat: 자기 소개

---
+++ src/component/introduce/Introduce.tsx
@@ -0,0 +1,21 @@
+import styles from "./Introduce.module.scss";
+
+import Bat from "../loading/bat/Bat";
+
+export default function Introduce() {
+  return (
+    <div className={styles["introduce-container"]}>
+      <Bat />
+
+      <p className={styles.p}>
+        📖 ☕️
+        <br />
+        책과 커피를 사랑하는 웹 개발자 박정우입니다.
+        <br />
+        표준화된 길보단 나만의 길을 추구합니다.
+        <br />
+        세상에 대한 호기심을 바탕으로 개발을 문제 해결의 도구로 활용합니다.
+      </p>
+    </div>
+  );
+}
