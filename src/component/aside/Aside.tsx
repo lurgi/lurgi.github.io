8e3475ab7907c17184f741f185e2508b31a33307
Jeongwoo Park <wahoo9040@naver.com>
2024-12-06 17:22:22 +0900

    feat: 사이드바 외부 링크 추가

--- src/component/aside/Aside.tsx
+++ src/component/aside/Aside.tsx
@@ -1,6 +1,22 @@
 import { Link } from "react-router";
-import styles from "./Aside.module.css";
+import styles from "./Aside.module.scss";
 import { postTypes } from "../../data";
+
+import LinkedInIcon from "../../assets/linkedin.svg?react";
+import GithubIcon from "../../assets/github.svg?react";
+
+const LINK = {
+  github: {
+    href: "https://github.com/lurgi",
+    icon: <GithubIcon width={30} height={30} />,
+  },
+  linkedIn: {
+    href: "https://www.linkedin.com/in/lurgi/",
+    icon: <LinkedInIcon width={34} height={34} />,
+  },
+} as const;
+
+const LINK_TYPES = Object.keys(LINK) as (keyof typeof LINK)[];
 
 export default function Aside() {
   return (
@@ -14,6 +30,13 @@ export default function Aside() {
             <li key={type}>
               <Link to={`/${type}`}>{type}</Link>
             </li>
+          ))}
+        </ul>
+        <ul className={styles["hyper-link-container"]}>
+          {LINK_TYPES.map((type) => LINK[type]).map(({ href, icon }, idx) => (
+            <a key={idx} className={styles["hyper-link"]} href={href} target="_blank">
+              {icon}
+            </a>
           ))}
         </ul>
       </nav>
