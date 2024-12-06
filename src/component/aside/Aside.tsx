55dd307ce1d4cb9ae2b39b1bc1f8f1fadc35ae1c
Jeongwoo Park <wahoo9040@naver.com>
2024-12-06 17:36:55 +0900

    feat: nav 링크 추가 및 하이라이트

--- src/component/aside/Aside.tsx
+++ src/component/aside/Aside.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import styles from "./Aside.module.scss";
 import { postTypes } from "../../data";
 
@@ -19,24 +19,33 @@ const LINK = {
 const LINK_TYPES = Object.keys(LINK) as (keyof typeof LINK)[];
 
 export default function Aside() {
+  const { pathname } = useLocation();
+
   return (
     <aside className={styles.aside}>
       <nav>
         <ul>
           <li>
-            <Link to={"/"}>Park Jeong Woo (lurgi)</Link>
+            <Link to={"/"}>
+              <p className={pathname === "/" ? styles["link-text-highlight"] : undefined}>Park Jeong Woo (lurgi)</p>
+            </Link>
           </li>
           {postTypes.map((type) => (
             <li key={type}>
-              <Link to={`/${type}`}>{type}</Link>
+              <Link to={`/${type}`}>
+                <p className={pathname === `/${type}` ? styles["link-text-highlight"] : undefined}>{type}</p>
+              </Link>
             </li>
           ))}
         </ul>
+
         <ul className={styles["hyper-link-container"]}>
           {LINK_TYPES.map((type) => LINK[type]).map(({ href, icon }, idx) => (
-            <a key={idx} className={styles["hyper-link"]} href={href} target="_blank">
-              {icon}
-            </a>
+            <li key={idx}>
+              <a className={styles["hyper-link"]} href={href} target="_blank">
+                {icon}
+              </a>
+            </li>
           ))}
         </ul>
       </nav>
--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -1,7 +1,8 @@
 import styles from "./Home.module.css";
 import { posts, postTypes } from "../../data";
 import PostPreview from "../preview/PostPreview";
-import Bat from "../loading/bat/Bat";
+import Introduce from "../introduce/Introduce";
+import { Link } from "react-router";
 
 interface PostListPart {
   type: PostType;
@@ -11,7 +12,9 @@ interface PostListPart {
 function PostPartList({ type, posts }: PostListPart) {
   return (
     <div className={styles.postPartList}>
-      <h2>{type}</h2>
+      <Link to={`/${type}`}>
+        <h2>{type}</h2>
+      </Link>
       {posts.map((post) => (
         <PostPreview url={`/${post.type}/${post.fileName}`} key={post.fileName} title={post.title} date={post.date} />
       ))}
@@ -22,15 +25,11 @@ function PostPartList({ type, posts }: PostListPart) {
 export default function Home() {
   return (
     <>
-      <div style={{ margin: "10rem 0rem" }}>
-        <Bat />
-        🐶안녕하세요!✋ 나중에 이곳에 소개가 들어갈거에요!
-        <br />
-        이곳에 깃헙 링크 링크드인 링크가 들어갈거에요!
-      </div>
+      <Introduce />
+
       <div className={styles.postList}>
-        {postTypes.slice(0, 5).map((key) => (
-          <PostPartList type={key} key={key} posts={posts[key]} />
+        {postTypes.map((key) => (
+          <PostPartList type={key} key={key} posts={posts[key].slice(0, 5)} />
         ))}
       </div>
     </>
