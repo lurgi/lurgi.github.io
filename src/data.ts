51513d253c6502ef9edef7327acf6a04adc000bc
Jeongwoo Park <wahoo9040@naver.com>
2024-12-06 20:14:10 +0900

    docs: 아티클 작성

--- src/data.ts
+++ src/data.ts
@@ -8,19 +8,24 @@ export const posts: Record<PostType, PostPreview[]> = {
     },
     {
       type: "Development",
+      title: "[Apollo Basic] Apollo Client를 이용하여 Supabase GraphQL 요청 보내기",
+      fileName: "Apollo-Basic-Apollo-Client-Using-Supabase-GraphQL-Request",
+      date: "2024-11-27",
+    },
+    {
+      type: "Development",
+      title: "[Settings] GraphQL과 Supabase, Apollo Client 설정",
+      fileName: "GraphQL-and-GraphQL-Apollo-Settings",
+      date: "2024-11-26",
+    },
+    {
+      type: "Development",
       title: "옵저버 패턴과 Tanstack Query에서의 옵저버 패턴",
       fileName: "Observer-Pattern-and-the-Use-of-Observer-Pattern-in-TanStack-Query",
       date: "2024-04-08",
     },
   ],
-  Reading: [
-    {
-      type: "Reading",
-      title: "이펙추에이션 - 사라스 사라스바티",
-      fileName: "Effectuation-Elements-of-Entrepreneurial-Expertise-Saras-D-Sarasvathy",
-      date: "2024-04-20",
-    },
-  ],
+
   Insights: [
     {
       type: "Insights",
@@ -39,6 +44,21 @@ export const posts: Record<PostType, PostPreview[]> = {
       title: "나 자신을 조금 더 드러내기",
       fileName: "Revealing-Myself-A-Little-More",
       date: "2024-03-26",
+    },
+  ],
+
+  Reading: [
+    {
+      type: "Reading",
+      title: "자유로부터의 도피 - 에리히 프롬",
+      fileName: "Escape-from-Freedom",
+      date: "2024-12-09",
+    },
+    {
+      type: "Reading",
+      title: "이펙추에이션 - 사라스 사라스바티",
+      fileName: "Effectuation-Elements-of-Entrepreneurial-Expertise-Saras-D-Sarasvathy",
+      date: "2024-04-20",
     },
   ],
 };
