1b0a888b8d8dfc72f3203b9e6908cd5988b88da0
Jeongwoo Park <wahoo9040@naver.com>
2024-12-06 12:34:06 +0900

    chore: svgr 플러그인 설치 및 svg 이미지 생성
    squash
    
    s

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,2 +1,4 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 type PostType = "Development" | "Reading" | "Insights";
 type PostPreview = { type: PostType; fileName: string; title: string; date: string };
