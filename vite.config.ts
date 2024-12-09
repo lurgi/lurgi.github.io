ccc9b9da358f13e7fc06252e7d1ed6e37bd02362
Jeongwoo Park <wahoo9040@naver.com>
2024-12-09 14:37:28 +0900

    chore: 테이블 스타일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdx from "@mdx-js/rollup";
 import svgr from "vite-plugin-svgr";
+import remarkGfm from "remark-gfm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx(), svgr()],
+  plugins: [react(), mdx({ remarkPlugins: [remarkGfm] }), svgr()],
 });
