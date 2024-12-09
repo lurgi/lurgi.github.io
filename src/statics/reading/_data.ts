b8404cb073d0f5e4220db444eddbc4f94ada6e44
Jeongwoo Park <wahoo9040@naver.com>
2024-12-09 14:00:09 +0900

    docs: 독서 기록

--- src/statics/reading/_data.ts
+++ src/statics/reading/_data.ts
@@ -1,15 +1,38 @@
 const READING: PostPreview[] = [
   {
     type: "Reading",
-    title: "자유로부터의 도피 - 에리히 프롬",
+    title: "자유로부터의 도피",
+    author: "에리히 프롬",
     fileName: "Escape-from-Freedom",
     date: "2024-12-09",
   },
   {
     type: "Reading",
-    title: "이펙추에이션 - 사라스 사라스바티",
-    fileName: "Effectuation-Elements-of-Entrepreneurial-Expertise-Saras-D-Sarasvathy",
+    title: "정보가 너무 많아서",
+    author: "캐스 R 선스타인",
+    fileName: "Too-Much-Information",
+    date: "2024-07-30",
+  },
+  {
+    type: "Reading",
+    title: "이펙추에이션",
+    author: "사라스 사라스바티",
+    fileName: "Effectuation-Elements-of-Entrepreneurial-Expertise",
     date: "2024-04-20",
+  },
+  {
+    type: "Reading",
+    title: "달과 6펜스",
+    author: "서머싯 몸",
+    fileName: "The-Moon-and-Sixpence",
+    date: "2023-08-20",
+  },
+  {
+    type: "Reading",
+    title: "물고기는 존재하지 않는다",
+    author: "룰루 밀러",
+    fileName: "Why-Fish-Dont-Exist",
+    date: "2023-06-12",
   },
 ];
 
