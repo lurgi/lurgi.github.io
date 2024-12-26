d58acd70694bb2c80564d51620569543242b4cb0
Jeongwoo Park <wahoo9040@naver.com>
2024-12-26 12:58:47 +0900

    build: workflow NODE_PATH 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "",
+  assetPrefix: "/",
   reactStrictMode: true,
   transpilePackages: ["react-syntax-highlighter"],
   experimental: {
