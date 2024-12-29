f5e3727f8eea32011c7a606cae1091a8c9edb39e
Jeongwoo Park <wahoo9040@naver.com>
2024-12-29 15:18:54 +0900

    feat: FancyImage 로딩상태 삭제

--- components/MDXComponents/FancyImage/FancyImage.tsx
+++ components/MDXComponents/FancyImage/FancyImage.tsx
@@ -1,25 +1,17 @@
-import { useState } from "react";
 import styles from "./FancyImage.module.css";
+import clsx from "clsx";
 
 interface FancyImageProps {
   alt: string;
   src: string;
 }
 
-export default function FancyImage(props: FancyImageProps) {
-  const [isLoading, setIsLoading] = useState(!!props.src);
-  const [isError, setIsError] = useState(false);
-
+export default function FancyImage({ alt, src }: FancyImageProps) {
   return (
-    isError || (
-      <span className={styles["image-container"]}>
-        <img
-          className={`${styles["responsive-image"]} ${isLoading && styles["image-loading"]}`}
-          onLoad={() => setIsLoading(false)}
-          onError={() => setIsError(true)}
-          {...props}
-        />
-      </span>
-    )
+    <span className={styles["image-container"]}>
+      <picture>
+        <img className={clsx(styles["responsive-image"], "image-loading")} alt={alt} src={src} />
+      </picture>
+    </span>
   );
 }
