abd4976ba6e4e23bcd0cc8a2b114488ec8986c4b
Jeongwoo Park <wahoo9040@naver.com>
2025-05-31 08:48:31 +0900

    feat: 빌드 타임 지연의 주범 글 삭제

--- src/statics/development/_data.ts
+++ src/statics/development/_data.ts
@@ -176,12 +176,6 @@ const DEVELOPMENT: PostPreview[] = [
   },
   {
     type: "Development",
-    title: "Paint 이전 Macro Task가 실행될 가능성과 React의 useEffect",
-    fileName: "Possibility-of-Macro-Tasks-Being-Executed-Before-Paint-and-React-useEffect",
-    date: "2024-11-28",
-  },
-  {
-    type: "Development",
     title: "[Apollo Basic] Apollo Client를 이용하여 Supabase GraphQL 요청 보내기",
     fileName: "Apollo-Basic-Apollo-Client-Using-Supabase-GraphQL-Request",
     date: "2024-11-27",
