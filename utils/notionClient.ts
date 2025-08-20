9fcf55ba6e56b98f249dcc034237e50827897db0
Jeongwoo Park <wahoo9040@naver.com>
2025-08-20 21:20:32 +0900

    feat: notion 업데이트 따른 endpoint 수정

--- utils/notionClient.ts
+++ utils/notionClient.ts
@@ -13,6 +13,21 @@ export const notionOfficialClient = new Client({
 export const notionClient = new NotionAPI({
   authToken: process.env.NOTION_TOKEN_V2,
   activeUser: process.env.NOTION_USER_ID,
+  kyOptions: {
+    hooks: {
+      beforeRequest: [
+        (request, options) => {
+          const url = request.url.toString();
+
+          if (url.includes("/api/v3/syncRecordValues")) {
+            return new Request(url.replace("/api/v3/syncRecordValues", "/api/v3/syncRecordValuesMain"), options);
+          }
+
+          return request;
+        },
+      ],
+    },
+  },
 });
 
 /* database에 따른 각 페이지 id를 가져오는 함수*/
