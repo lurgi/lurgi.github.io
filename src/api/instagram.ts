b4b5a9706f7ade6c098ca8bea2161aafb378747a
Jeongwoo Park <wahoo9040@naver.com>
2025-08-22 07:41:43 +0900

    chore: Instagram API 오류 메시지에 상태 텍스트 추가

--- src/api/instagram.ts
+++ src/api/instagram.ts
@@ -4,7 +4,7 @@ export const getMediaIds = async (): Promise<{ id: string }[]> => {
   const ACCESS_TOKEN = process.env.NEXT_PUBLIC_INSTAGRAM_ACCESS_TOKEN;
   const res = await fetch(`${INSTAGRAM_END_POINT}/me/media?fields=id&access_token=${ACCESS_TOKEN}`);
   if (!res.ok) {
-    throw new Error("미디어를 불러오는데 실패했습니다.");
+    throw new Error(`미디어를 불러오는데 실패했습니다. ${res.statusText}`);
   }
   const data = await res.json();
   return data.data;
@@ -23,7 +23,7 @@ export const getMediaDetail = async ({ instagramId }: { instagramId: string }): 
     `${INSTAGRAM_END_POINT}/${instagramId}/children?fields=media_url,media_type,timestamp&access_token=${ACCESS_TOKEN}}`
   );
   if (!res.ok) {
-    throw new Error("미디어를 불러오는데 실패했습니다.");
+    throw new Error(`미디어를 불러오는데 실패했습니다. ${res.statusText}`);
   }
   const data = await res.json();
   return data.data;
