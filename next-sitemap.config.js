ca42c88243835fb66565b6e11002ed646e77159d
Jeongwoo Park <wahoo9040@naver.com>
2026-01-30 19:42:20 +0900

    chore: 사이트맵 인덱스 기능 삭제

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,18 @@ module.exports = {
   siteUrl: "https://lurgi.github.io",
   generateRobotsTxt: true,
   outDir: "./out",
+  generateIndexSitemap: false,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/api/*"],
+  transform: async (config, path) => {
+    const notionPath = "notion/";
+    const isNotionPath = path.includes(notionPath);
+
+    if (isNotionPath) {
+      return { loc: path, priority: 0.8, changefreq: "weekly" };
+    }
+    return { loc: path, priority: 0.7, changefreq: "weekly" };
+  },
 };
