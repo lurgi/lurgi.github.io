d2915eff15794b62f24c41aa453b60b878f8dd05
Jeongwoo Park <wahoo9040@naver.com>
2026-09-01 22:48:14 +0900

    feat: 네이버 사이트 검증 메타 태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta
+          name="naver-site-verification"
+          content="dcbbdb21fb8f39a9355a9ee3b7e5bff7c7885ed8"
+        />
         <script
           defer
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
